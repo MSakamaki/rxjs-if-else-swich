6b74f967a0faf1c28d1fe44b3b0f253537c225f4
MSakamaki <mail@m-sakamaki.jp>
2019-07-06 00:48:01 +0900

    1 file modified

--- index.ts
+++ index.ts
@@ -1,26 +1,26 @@
 import { of, from, Observable, merge } from 'rxjs'; 
 import { map, filter, tap, switchMap } from 'rxjs/operators';
 
-const source = from([1,2,3,4,5,6,7,8,9,10]);
-
-const caseWhen =
-  (match: number, caseFnc: ()=> string ) => (imput$: Observable<number>): Observable<string> => imput$.pipe(
-    filter(num => num === match),
-    map(caseFnc)
-  );
-merge(
-  source.pipe(caseWhen(1, ()=> '.')),
-  source.pipe(caseWhen(2, ()=> '..')),
-  source.pipe(caseWhen(3, ()=> '...')),
-  source.pipe(caseWhen(4, ()=> '....')),
-  source.pipe(caseWhen(5, ()=> '.....')),
-).subscribe(console.log);
-
 interface Param {
   min: number;
   max: number;
   result: string;
 }
+
+const source = from([1,2,3,4,5,6,7,8,9,10]);
+
+
+const caseWhen =
+  (min: number, max: number, result:string ) => (imput$: Observable<number>): Observable<string> => imput$.pipe(
+    filter(n =>  min <= n && max >= n),
+    map(()=> result)
+  );
+merge(
+  source.pipe(caseWhen(1, 3, '.')),
+  source.pipe(caseWhen(2, 5, '..')),
+  source.pipe(caseWhen(8, 9, '..')),
+).subscribe(console.log);
+
 
 const mergeWhen = (params:Param[]) => (imput$: Observable<number>): Observable<string> =>
   imput$.pipe(
