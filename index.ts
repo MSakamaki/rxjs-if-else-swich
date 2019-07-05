4c5245584f8e20fc307ece84cfa21813edacd23b
MSakamaki <mail@m-sakamaki.jp>
2019-07-06 00:10:41 +0900

    1 file modified

--- index.ts
+++ index.ts
@@ -2,43 +2,40 @@ import { of, from, Observable, merge } from 'rxjs';
 import { map, filter, tap, switchMap } from 'rxjs/operators';
 
 
-const source = from([1,2,3,4,5,6]);
+const source = from([1,2,3,4,5,6,7,8,9,10]);
 
-// const caseWhen =
-//   (match: number, caseFnc: ()=> string ) => (imput$: Observable<number>): Observable<string> => imput$.pipe(
-//     filter(num => num === match),
-//     map(caseFnc)
-//   );
-// merge(
-//   source.pipe(caseWhen(1, ()=> 'one')),
-//   source.pipe(caseWhen(2, ()=> '||')),
-//   source.pipe(caseWhen(3, ()=> '三')),
-//   source.pipe(caseWhen(4, ()=> 'フォー')),
-//   source.pipe(caseWhen(5, ()=> 'ご')),
-// ).subscribe(console.log);
+const caseWhen =
+  (match: number, caseFnc: ()=> string ) => (imput$: Observable<number>): Observable<string> => imput$.pipe(
+    filter(num => num === match),
+    map(caseFnc)
+  );
+merge(
+  source.pipe(caseWhen(1, ()=> '-')),
+  source.pipe(caseWhen(2, ()=> '--')),
+  source.pipe(caseWhen(3, ()=> '---')),
+  source.pipe(caseWhen(4, ()=> '----')),
+  source.pipe(caseWhen(5, ()=> '-----')),
+).subscribe(console.log);
 
-const mergeWhen = (csd: {
-  match: number;
-  fnc: ()=> string;
-}[]) => (imput$: Observable<number>): Observable<string> =>
+interface Param {
+  min: number;
+  max: number;
+  result: string;
+}
+
+const mergeWhen = (params:Param[]) => (imput$: Observable<number>): Observable<string> =>
   imput$.pipe(
-    switchMap(num => csd
-      .filter(cs => cs.match === num)
-      .map( cs => cs.fnc()))
+    map(n => params.find(param => param.min <= n && param.max >= n)),
+    filter(Boolean),
+    map(cs => cs.result)
   );
 
 source.pipe(
   mergeWhen([{
-    match: 1, fnc: () => '|'
+    min: 1, max: 3, result: '|'
   }, {
-    match: 2, fnc: () => '||'
+    min: 6, max: 8, result: '--'
   }, {
-    match: 3, fnc: () => '|||'
-  }, {
-    match: 4, fnc: () => '||||'
-  }, {
-    match: 5, fnc: () => '|||||'
-  }, {
-    match: 6, fnc: () => '||||||'
+    min: 7, max: 10, result: '/'
   }])
 ).subscribe(console.log);
